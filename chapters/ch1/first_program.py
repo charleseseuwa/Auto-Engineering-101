022b7fac8eb02f223e17a5f32f62028d34ef0813
Charles <charlieu2000@gmail.com>
2024-03-24 08:23:52 +0100

    challange1

---
+++ chapters/ch1/first_program.py
@@ -0,0 +1,10 @@
+
+my_name = "Christabel uwagboe"
+
+sentence = "A Cow jumped over the moon"
+
+
+first_name = input("Enter your first name")
+last_name = input("Enter your last name")
+
+print(f"My full name is {first_name} {last_name}")
